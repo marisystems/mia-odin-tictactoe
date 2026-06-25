f51abfa0b0aaa4085fb5ded7587d9cdcdf4eb3c3
marisystems <215432766+marisystems@users.noreply.github.com>
2026-06-25 15:50:06 -0300

    Finish win condition for tic-tac-toe
    End up abandoning the magic square in favour of just summing the lines,
    columns and diagonasl of the matrix to check for winning combinations!

--- script.js
+++ script.js
@@ -2,12 +2,6 @@
 // Functional tic-tac-toe game first in the console
 // And only after that worry about html&css
 
-// SO THE ISSUE RIGHT NOW:
-// I have to check for a win condition, and in a way when it goes over 15
-// I'm thinking of looping the 2d array and the values of each row and column
-// and check if THAT goes over 15, but now that I think the magicSquare is
-// not being modified, and the playing array is different
-
 function create2DArray(rows, cols, fill) {
     // Helper function to create the gameboard
     const myArray = [];
@@ -21,91 +15,20 @@ function create2DArray(rows, cols, fill) {
     return myArray
 }
 
-function sumMagicArray(board, magicBoard, player) {
-    // I'm sure there's much more elegants solutions out there, but I may be stoopid
-    let obj = {
-        line0: 0,
-        line1: 0,
-        line2: 0,
-        col0: 0,
-        col1: 0,
-        col2: 0,
-        diagM: 0,
-        diagS: 0,
-    };
 
-    // Get positions from board to use the magic board
-
-    let len = board.length;
-
-    for(let i = 0; i < len; i++) {
-        for(let j = 0; j < len; j++) {
-
-            // Get each line
-            switch(i) {
-                case 0:
-                    obj["line0"] += arr[i][j]
-                    break;
-                case 1:
-                    obj["line1"] += arr[i][j]
-                    break;
-                case 2:
-                    obj["line2"] += arr[i][j]
-                    break;
-            }
-
-            // Get each column
-            switch(j) {
-                
-                case 0:
-                    obj["col0"] += arr[i][j]
-                    break;
-                case 1:
-                    obj["col1"] += arr[i][j]
-                    break;
-                case 2:
-                    obj["col2"] += arr[i][j]
-                    break;
-            }
-
-            // Main diagonal
-            if (i === j) {
-                // console.log(`Main diagonal: ${arr[i][j]}`)
-                obj["diagM"] += arr[i][j];
-            }
-
-            // Secondary diagonal
-            if (i + j == len - 1) {
-                // console.log(`Secondary diagonal: ${arr[i][j]}`)
-                obj["diagS"] += arr[i][j];
-            }
-        }
-    }
-    return obj;
-}
 
 // Make an IIFE for GameBoard since we'll only declare it once
 const GameBoard = ( () => {
 
     let board = create2DArray(3, 3, "");
 
-    const magicSquare = [
-        [2,7,6],
-        [9,5,1],
-        [4,3,8]
-    ]
-
     // Function that returns the gameBoard status
     const getBoard = () => board
-    const getMagic = () => magicSquare;
 
-    return {getBoard, getMagic};
+    return {getBoard};
 } ) ()
 
-
 const GameController = ( () => {
-    // This will return functions to change the state of the game
-    // depending on the player and their check type
 
     function changeBoard([i, j], value, board){
         if (board[i][j] != '') {
@@ -115,35 +38,92 @@ const GameController = ( () => {
         }
     }
 
-    function playRound([i,j], player, board, magicBoard) {
+    function playRound([i,j], player, board) {
         try { 
             changeBoard([i,j], player.getCheck(), board);
-            let magicValue = magicBoard[i][j];
-            player.magicSum(magicValue);
-        
             console.log(`${player.getName()} has played at ${[i,j]} with ${player.getCheck()}`);
             console.table(board);
-            checkWinner(player)
+            checkWinner(board, player)
         } catch (e) {
             console.log(e)
         }
     }
 
-    function checkWinner(player) {
-        // console.log("Checking winner")
-        if (player.getSum() == 15) {
-            player.incrementScore();
-            console.log(`${player.getName()}: Wins!`)
-            console.log(`Score: ${player.getScore()}`)
+    function checkWinner(board, player) {
+        // I'm sure there's much more elegants solutions out there, but I may be stoopid
+
+        let check = player.getCheck();
+        let winCondition = check + check + check;
+        console.log(`Win condition: ${winCondition}`)
+
+        let obj = {
+            line0: '',
+            line1: '',
+            line2: '',
+            col0: '',
+            col1: '',
+            col2: '',
+            diagM: '',
+            diagS: '',
+        };
+
+        let len = board.length;
+
+        for(let i = 0; i < len; i++) {
+            for(let j = 0; j < len; j++) {
+
+                // Get each line
+                switch(i) {
+                    case 0:
+                        obj["line0"] += board[i][j]
+                        break;
+                    case 1:
+                        obj["line1"] += board[i][j]
+                        break;
+                    case 2:
+                        obj["line2"] += board[i][j]
+                        break;
+                }
+                // Get each column
+                switch(j) {
+                    case 0:
+                        obj["col0"] += board[i][j]
+                        break;
+                    case 1:
+                        obj["col1"] += board[i][j]
+                        break;
+                    case 2:
+                        obj["col2"] += board[i][j]
+                        break;
+                }
+                // Main diagonal
+                if (i === j) {
+                    // console.log(`Main diagonal: ${arr[i][j]}`)
+                    obj["diagM"] += board[i][j];
+                }
+                // Secondary diagonal
+                if (i + j == len - 1) {
+                    // console.log(`Secondary diagonal: ${arr[i][j]}`)
+                    obj["diagS"] += board[i][j];
+                }
+            }
+        }
+        // If a line or column or diagonal has either 3 O's or 3X's declare wiiner
+
+        for (const key in obj) {
+            const element = obj[key];
+            
+            if (element === winCondition) {
+                console.log(`${player.getName()} wins!`)
+            }
         }
     }
 
-    return {changeBoard, playRound, checkWinner};
+    return {changeBoard, playRound};
 })()
 
 const Player = (name, checkType) => {
     let score = 0;
-    let sum = 0;
 
     // Map the checks to either Cross or Circle
     if (checkType.toLowerCase() === "cross") {
@@ -160,9 +140,7 @@ const Player = (name, checkType) => {
         console.log(`Player: ${name} | Check: ${checkType} | Score: ${score}`);
     }
     const getScore = () => score;
-    const getSum = () => sum;
     const incrementScore = () => score++;
-    const magicSum = (magicValue) => sum += magicValue;
 
     return {
         getName, 
@@ -170,23 +148,22 @@ const Player = (name, checkType) => {
         getInfo, 
         getScore,
         incrementScore, 
-        magicSum, 
-        getSum
     };
 }
 
 let player1 = Player("Mari", "Cross");
 let player2 = Player("John", "Circle")
 let board = GameBoard.getBoard();
-const magicBoard = GameBoard.getMagic();
 
-GameController.playRound([0,0], player2, board, magicBoard);
-GameController.playRound([1,1], player1, board, magicBoard);
-GameController.playRound([1,0], player2, board, magicBoard);
-GameController.playRound([2,0], player1, board, magicBoard);
-GameController.playRound([0,1], player2, board, magicBoard);
-GameController.playRound([2,1], player1, board, magicBoard);
-GameController.playRound([0,2], player2, board, magicBoard);
-console.log(player2.getSum())
+GameController.playRound([0,0], player2, board);
 
-console.log(board)
+GameController.playRound([2,2], player1, board);
+
+GameController.playRound([1,0], player2, board);
+
+GameController.playRound([2,0], player1, board);
+
+GameController.playRound([0,1], player2, board);
+
+GameController.playRound([2,1], player1, board);
+
